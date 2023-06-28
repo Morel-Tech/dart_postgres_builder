5193c2dbb33d855a3e6b13c23d4fd68005ae5c9a
Marcus Twichel <marc.twichy@gmail.com>
2023-06-28 17:19:10 -0600

    feat: add joins (#5)
    * feat: add joins
    
    * update very good analysis
    
    * remove paramater
    
    * fix spelling

--- lib/src/statements/equals.dart
+++ lib/src/statements/equals.dart
@@ -1,16 +1,29 @@
 import 'package:postgres_builder/postgres_builder.dart';
 
 class Equals implements FilterStatement {
-  const Equals(this.column, this.value);
+  const Equals(this.column, this.value) : useParameter = true;
+  const Equals.otherColumn(
+    Column column1,
+    Column column2,
+  )   : column = column1,
+        value = column2,
+        useParameter = false;
 
-  final String column;
+  final Column column;
   final dynamic value;
+  final bool useParameter;
 
   @override
   ProcessedSql toSql() {
+    if (useParameter) {
+      return ProcessedSql(
+        query: '$column = @$column',
+        parameters: {column.toString(): value},
+      );
+    }
     return ProcessedSql(
-      query: '$column = @$column',
-      parameters: {column: value},
+      query: '$column = $value',
+      parameters: {},
     );
   }
 }

1d7937e613452472bb4b546d5d4a1e682a18bbaa
Marcus Twichel <marcus@morel.technology>
2023-10-28 14:29:42 -0600

    fix: group by error (#15)
    * fix: group by error
    
    * version bump

--- test/src/statements/group_test.dart
+++ test/src/statements/group_test.dart
@@ -1,18 +1,13 @@
-import 'package:mocktail/mocktail.dart';
 import 'package:postgres_builder/postgres_builder.dart';
 import 'package:test/test.dart';
 
 import '../../_helpers.dart';
 
-class _MockColumn extends Mock implements Column {}
-
 void main() {
   group('Group', () {
     test('.toSql returns correctly', () {
-      final column1 = _MockColumn();
-      final column2 = _MockColumn();
-      when(() => column1.parameterName).thenReturn('__column1__');
-      when(() => column2.parameterName).thenReturn('__column2__');
+      const column1 = Column('__column1__');
+      const column2 = Column('__column2__');
       expect(
         Group([column1, column2]).toSql(),
         equalsSql(query: 'GROUP BY __column1__, __column2__'),

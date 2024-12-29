4ac75d988d77cc94a6d6f8f73a7bfa97ece91b44
Marcus Twichel <marc.twichy@gmail.com>
2024-12-28 23:49:56 -0700

    fix code coverage

--- test/src/postgres_builder_exception_test.dart
+++ test/src/postgres_builder_exception_test.dart
@@ -2,11 +2,18 @@ import 'package:postgres_builder/postgres_builder.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('PostgresBuilderException', () {
+  group('$PostgresBuilderException', () {
     test('can be created', () {
       expect(
         const PostgresBuilderException('__message__'),
         isA<PostgresBuilderException>(),
+      );
+    });
+
+    test('toString() returns correctly', () {
+      expect(
+        const PostgresBuilderException('__message__').toString(),
+        equals('PostgresBuilderException: __message__\ndetails: null'),
       );
     });
   });
